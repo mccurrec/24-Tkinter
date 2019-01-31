304172afb29da87838cb3db010777b8090ed80f7
mccurrec <mccurrec@rose-hulman.edu>
2019-01-31 15:55:52 -0500

    done

--- src/m1e_root_and_mainloop.py
+++ src/m1e_root_and_mainloop.py
@@ -7,15 +7,16 @@ A window should pop up.  That's all for this demo.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Ezrie McCurry.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import tkinter
 from tkinter import ttk  # Necessary in all but this trivial example.
 
 
 def main():
-    root = tkinter.Tk()
+    # Tk is a tkinter object
+    root = tkinter.Tk() # only create one Tk object, multiple windows can be created with one Tk object
     root.mainloop()
 
     print('Done with the Event Loop')  # Note when this line runs.
